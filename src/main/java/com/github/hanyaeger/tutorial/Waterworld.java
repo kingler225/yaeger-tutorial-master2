feacfe19ea0de36a1aa956035d2f376de06d1869
kingler225 <102318939+kingler225@users.noreply.github.com>
2022-04-07 20:35:39 +0200

    yaegertut

---
+++ src/main/java/com/github/hanyaeger/tutorial/Waterworld.java
@@ -0,0 +1,27 @@
+package com.github.hanyaeger.tutorial;
+
+import com.github.hanyaeger.api.Size;
+import com.github.hanyaeger.api.YaegerGame;
+import com.github.hanyaeger.tutorial.scenes.GameLevel;
+import com.github.hanyaeger.tutorial.scenes.TitleScene;
+
+public class Waterworld extends YaegerGame {
+    @Override
+    public void setupGame() {
+        setGameTitle("Waterworld");
+        setSize(new Size(800, 600));
+
+    }
+
+    @Override
+    public void setupScenes() {
+        addScene(0, new TitleScene());
+        addScene(1, new GameLevel());
+
+    }
+
+    public static void main(String[] args){
+        launch(args);
+    }
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module waterworld {
+    requires hanyaeger;
+
+    exports com.github.hanyaeger.tutorial;
+
+    opens audio;
+    opens backgrounds;
+    opens sprites;
+    exports com.github.hanyaeger.tutorial.scenes;
+}
